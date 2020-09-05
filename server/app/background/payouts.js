4ae17395165e0a209a95ee95409e4adfb16c4aa2
David Brooks <david@colab.coop>
2020-09-04 19:23:30 -0500

    disabling settlement

--- server/app/background/payouts.js
+++ server/app/background/payouts.js
@@ -73,7 +73,7 @@ module.exports = () => {
   setTimeout(async() => {
     try {
       await disburse();
-      await settle();
+      // await settle();
     } catch(err) {
       logger.error('Error in payouts background job: %s', err);  
     }
