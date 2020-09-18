521e1ab3678e4b029f85a1b5203f9a3b72ef09ff
Gary Saunders <gary.saunders@sportsengine.com>
2020-09-18 10:10:10 -0400

    Removing extraneous implementation that I forgot I had committed.

--- server/app/lib/sms.js
+++ server/app/lib/sms.js
@@ -1,7 +1,6 @@
 import logger from 'logops';
 import { international as phoneFormat } from './phone';
 import { ov_config } from './ov_config';
-import { carrier } from './carrier';
 
 const client = require('twilio')(ov_config.twilio_account_sid, ov_config.twilio_auth_token, { 
     lazyLoading: true 
@@ -13,19 +12,6 @@ module.exports = (to, message) => {
   if (ov_config.twilio_disable) {
     logger.debug('Bypassing sending SMS');
     return;
-  }
-
-  try {
-    let lookup = await carrier(to);
-    let blockedCarriers = ov_config.blocked_carriers
-    let blockTest = new RegExp( blockedCarriers.join("|"), "i");
-    let isBlocked = blockTest.test(lookup.carrier.name);
-    if(isBlocked) {
-      logger.info(`Blocked sending message to ${phoneFormat(to)} because carrier ${lookup.carrier.name} is blocked in config`);
-      throw `Blocked sending message to ${phoneFormat(to)} because carrier ${lookup.carrier.name} is blocked in config`;
-    }
-  } catch(err) {
-    logger.info(`Won't send SMS because ${err}`)
   }
 
   return client.messages.create({from: ov_config.twilio_from, to: phoneFormat(to), body: message});
