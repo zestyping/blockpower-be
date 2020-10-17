9d5d719dad6af129ca89cd552f6e209df2c28a3d
David Brooks <david@colab.coop>
2020-10-17 09:55:42 -0500

    changing distance constraint on tripler search

--- server/app/services/triplers.js
+++ server/app/services/triplers.js
@@ -368,7 +368,7 @@ async function searchTriplersAmbassador(req) {
     limit 500
     match(a:Ambassador{id:"${req.user.get('id')}"})
     with a.location as a_location, node, apoc.text.levenshteinSimilarity(replace(replace(toLower(node.full_name),'-',''),"'",''), first_n_q + ' ' + last_n_q) as score1, apoc.text.jaroWinklerDistance(replace(replace(toLower(node.full_name),'-',''),"'",''), first_n_q + ' ' + last_n_q) as score2, apoc.text.sorensenDiceSimilarity(replace(replace(toLower(node.full_name),'-',''),"'",''), first_n_q + ' ' + last_n_q) as score3
-    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/10000 as distance
+    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/1000 as distance
     with node, avg_score / distance as final_score
     return node
     order by final_score desc limit 100
@@ -383,7 +383,7 @@ async function searchTriplersAmbassador(req) {
     limit 500
     match(a:Ambassador{id:"${req.user.get('id')}"})
     with a.location as a_location, node, apoc.text.levenshteinSimilarity(replace(replace(toLower(node.first_name),'-',''),"'",''), first_n_q) as score1, apoc.text.jaroWinklerDistance(replace(replace(toLower(node.first_name),'-',''),"'",''), first_n_q) as score2, apoc.text.sorensenDiceSimilarity(replace(replace(toLower(node.first_name),'-',''),"'",''), first_n_q) as score3
-    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/10000 as distance
+    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/1000 as distance
     with node, avg_score / distance as final_score
     return node
     order by final_score desc, node.last_name limit 100
@@ -398,7 +398,7 @@ async function searchTriplersAmbassador(req) {
     limit 500
     match(a:Ambassador{id:"${req.user.get('id')}"})
     with a.location as a_location, node, apoc.text.levenshteinSimilarity(replace(replace(toLower(node.last_name),'-',''),"'",''), last_n_q) as score1, apoc.text.jaroWinklerDistance(replace(replace(toLower(node.last_name),'-',''),"'",''), last_n_q) as score2, apoc.text.sorensenDiceSimilarity(replace(replace(toLower(node.last_name),'-',''),"'",''), last_n_q) as score3
-    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/10000 as distance
+    with node, (score1 + score2 + score3) / 3 as avg_score, distance(a_location, node.location)/1000 as distance
     with node, avg_score / distance as final_score
     return node
     order by final_score desc, node.first_name limit 100
