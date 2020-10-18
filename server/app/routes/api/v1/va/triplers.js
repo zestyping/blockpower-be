9503d5f012513f53f785a7218e8b5ac8d0d7f51a
Kevin Cooper <cooperka92@gmail.com>
2020-10-18 12:06:39 -0700

    Remove obsolete suggest-triplers endpoint

--- server/app/routes/api/v1/va/triplers.js
+++ server/app/routes/api/v1/va/triplers.js
@@ -89,55 +89,6 @@ async function searchTriplersAdmin(req, res) {
   return res.json(models);
 }
 
-// provide a list of potential triplers for an ambassador to select from
-async function suggestTriplers(req, res) {
-  let exclude_except = '';
-
-  if (ov_config.exclude_unreg_except_in) {
-    exclude_except += ov_config.exclude_unreg_except_in.split(",").map((state) => {
-      return `AND NOT t.address CONTAINS '\"state\": \"${state}\"' `
-    }).join(' ')
-  }
-
-  // TODO: remove this endpoint and use Lee's separate query in search
-  let collection = await req.neode.query()
-    .match('a', 'Ambassador', {id: req.user.get('id')})
-    .with('a, apoc.convert.fromJsonMap(a.address) as address')
-    .with('a, toString(address.zip) as zip')
-    .match('t', 'Tripler')
-    .where('t.zip starts with left(zip,3)')
-    .with('a,t')
-    .whereRaw('NOT ()-[:CLAIMS]->(t)')
-    .whereRaw('NOT ()-[:WAS_ONCE]->(t)')
-    // .whereRaw(`NOT t.voter_id CONTAINS "Unreg" ${exclude_except}`)
-    .with('a, t, distance(t.location, a.location) AS distance')
-    .orderBy('distance')
-    .return('t, distance')
-    .limit(ov_config.suggest_tripler_limit)
-    .execute()
-/*
-    .match('a', 'Ambassador')
-    .where('a.id', req.user.get('id'))
-    .match('t', 'Tripler')
-    .whereRaw('NOT ()-[:CLAIMS]->(t)')
-    .whereRaw('NOT ()-[:WAS_ONCE]->(t)')
-    .whereRaw(`NOT t.voter_id CONTAINS "Unreg" ${exclude_except}`)
-    .whereRaw(`distance(t.location, a.location) <= ${ov_config.ambassador_tripler_relation_max_distance}`) // distance in meters (see .env)
-    .with('a, t, distance(t.location, a.location) AS distance')
-    .orderBy('distance')
-    .return('t, distance')
-    .limit(ov_config.suggest_tripler_limit)
-    .execute()
-*/
-  let models = [];
-  for (var index = 0; index < collection.records.length; index++) {
-    let entry = collection.records[index]._fields[0].properties;
-    entry['distance'] = collection.records[index]._fields[1];
-    models.push(serializeNeo4JTripler(entry));
-  }
-  return res.json(models);
-}
-
 async function fetchTripler(req, res) {
   let ambassador = req.user;
   let tripler = null;
@@ -361,10 +312,6 @@ module.exports = Router({mergeParams: true})
   } else {
     return searchTriplersAmbassador(req, res);
   }
-})
-.get('/suggest-triplers', (req, res) => {
-  if (!req.authenticated) return _401(res, 'Permission denied.');
-  return suggestTriplers(req, res);
 })
 .put('/triplers/:triplerId/start-confirm', (req, res) => {
   if (!req.authenticated) return _401(res, 'Permission denied.');
