754936c1e49a4861684b0bdcde1a041cdd887cb0
David Brooks <david@colab.coop>
2020-10-19 13:31:43 -0500

    Revert "Clean up error email too"
    This reverts commit bea6f2fe8281f308799b22cbe16799307d45b4e4.

--- server/app/services/errors.js
+++ server/app/services/errors.js
@@ -1,14 +1,14 @@
 import mail from '../lib/mail';
 import { ov_config } from '../lib/ov_config';
-import { _400, _404, _500 } from '../lib/utils';
+import {
+  _204, _400, _401, _403, _404, _500, geoCode
+} from '../lib/utils';
 
 async function error(code, res, err, details) {
   try {
-    const subject = `Error ${code}: ${err}`;
-    const body = details
-      ? `Error ${code}: ${err}\n\n${JSON.stringify(details)}`
-      : `Error ${code}: ${err}`;
-    await mail(ov_config.admin_emails, null, null, subject, body);
+    await mail(ov_config.admin_emails, null, null,
+                 `Error ${code}: ${err}`,
+                 details ? `Error ${code}: ${err}\n\n${JSON.stringify(details)}`: `Error ${code}: ${err}`);
   } catch (err) {
     throw "Error sending email to admin on error"
   }
