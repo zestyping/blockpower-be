f6a45b4bbd554feb64867003da6170ad2b2e4da6
Cristina <ce@tsl.io>
2020-11-24 15:54:30 -0500

    updating w9 endpoint

--- server/app/models/va/Ambassador.js
+++ server/app/models/va/Ambassador.js
@@ -6,7 +6,7 @@ module.exports = {
   // This is the BlockPower internal ID (uuidv4)
   id: {
     type: 'uuid',
-    primary: true
+    primary: true,
   },
   // This is the ID given by Facebook or Google
   external_id: {
@@ -14,28 +14,28 @@ module.exports = {
   },
   first_name: {
     type: 'string',
-    required: true
+    required: true,
   },
   last_name: {
     type: 'string',
-    required: true
+    required: true,
   },
   date_of_birth: 'string',
   phone: {
     type: 'string',
-    required: true
+    required: true,
   },
   email: {
     type: 'string',
   },
   address: {
     type: 'string',
-    required: true
+    required: true,
   },
   // This is the lat/lon data obtained either through address or zip
   location: {
     type: 'point',
-    required: true
+    required: true,
   },
   // This Neo4J relationship says that this Ambassador has "claimed" this Tripler.
   // The Ambassador is expected to send the SMS to the Tripler asking to confirm.
@@ -49,32 +49,32 @@ module.exports = {
     properties: {
       since: {
         type: 'localdatetime',
-        default: () => new Date,
+        default: () => new Date(),
       },
     },
-    eager: true
+    eager: true,
   },
   signup_completed: {
     type: 'boolean',
-    default: false
+    default: false,
   },
   // This is obsolete. It was used when "onboarding" an Ambassador was done.
   onboarding_completed: {
     type: 'boolean',
-    default: false
+    default: false,
   },
   // If approved (automatically set to true on signup) the Ambassador is able
   //   to perform all Ambassador functions. If disapproved (by way of the admin panel)
   //   then that Ambassador is not able to perform most functions.
   approved: {
     type: 'boolean',
-    default: false
+    default: false,
   },
   // This is obsolete. It was used when "onboarding" an Ambassador was done.
   quiz_results: 'string',
   created_at: {
     type: 'localdatetime',
-    default: () => new Date,
+    default: () => new Date(),
   },
   // This is a carry-over from the HelloVoter "Volunteer" model. It was used there
   //   to prevent an account from logging in. It is similarly used in the same way
@@ -83,17 +83,21 @@ module.exports = {
   //   approval/disapproval ambassador admin functions.
   locked: {
     type: 'boolean',
-    default: false
+    default: false,
   },
   // This is obsolete. It was used prior to separate Account models being implemented.
   payout_provider: 'string',
   // Determines if the user is an Admin or not.
   admin: {
     type: 'boolean',
-    default: false
+    default: false,
   },
   // This relationship connects an Ambassador to a Payout. An Ambassador can have many
   //   Payouts. NOTE the tripler_id property of this relationship.
+  has_w9: {
+    type: 'boolean',
+    default: false,
+  },
   gets_paid: {
     type: 'relationships',
     relationship: 'GETS_PAID',
@@ -102,11 +106,11 @@ module.exports = {
     properties: {
       since: {
         type: 'localdatetime',
-        default: () => new Date,
+        default: () => new Date(),
       },
-      tripler_id: 'uuid'
+      tripler_id: 'uuid',
     },
-    eager: true
+    eager: true,
   },
   // This relationship is obsolete and unused.
   first_reward: {
@@ -117,11 +121,11 @@ module.exports = {
     properties: {
       since: {
         type: 'localdatetime',
-        default: () => new Date,
+        default: () => new Date(),
       },
-      tripler_id: 'uuid'
+      tripler_id: 'uuid',
     },
-    eager: true
+    eager: true,
   },
   // This relationship connects an Ambassador to an Account. An Ambassador can have many
   //   different Accounts.
@@ -133,10 +137,10 @@ module.exports = {
     properties: {
       since: {
         type: 'localdatetime',
-        default: () => new Date,
-      }
+        default: () => new Date(),
+      },
     },
-    eager: true
+    eager: true,
   },
   // This relatinship is between an Ambassador and a Tripler. It signifies that an Ambassador
   //   was once the tripler this relationship connects with. This means the tripler has "upgraded"
@@ -152,15 +156,14 @@ module.exports = {
       rewarded_previous_claimer: 'boolean',
       since: {
         type: 'localdatetime',
-        default: () => new Date
-      }
+        default: () => new Date(),
+      },
     },
-    eager: true
+    eager: true,
   },
   // This contains the stringified JSON response from Twilio and Ekata's caller ID services
   verification: 'string',
   // This contains the stringified JSON response from Twilio on the carrier data for this
   //   Ambassador's phone number
-  carrier_info: 'string'
-};
-
+  carrier_info: 'string',
+}
--- server/app/routes/api/v1/va/serializers.js
+++ server/app/routes/api/v1/va/serializers.js
@@ -1,5 +1,5 @@
-import { formatDate, formatNumber } from '../../../../lib/format';
-import PhoneNumber from 'awesome-phonenumber';
+import {formatDate, formatNumber} from '../../../../lib/format'
+import PhoneNumber from 'awesome-phonenumber'
 
 /*
  *
@@ -8,42 +8,68 @@ import PhoneNumber from 'awesome-phonenumber';
  */
 
 function serializeAddress(address) {
-  if (!address) return '';
-  let keys = [ 'address1', 'city', 'state', 'zip' ];
-  let values = [];
-  keys.forEach((key)=>{if (address[key]) values.push(address[key])});
-  return values.join(", ");
+  if (!address) return ''
+  let keys = ['address1', 'city', 'state', 'zip']
+  let values = []
+  keys.forEach((key) => {
+    if (address[key]) values.push(address[key])
+  })
+  return values.join(', ')
 }
 
 function serializeName(first_name, last_name) {
-  if (!last_name) return first_name;
-  return [first_name, last_name].join(" ");
+  if (!last_name) return first_name
+  return [first_name, last_name].join(' ')
 }
 
 function serializeAccount(account) {
-  let obj = {};
-  ['id', 'account_id', 'account_type'].forEach(x => obj[x] = account.get(x));
-  obj['account_data'] = !!account.get('account_data') ? JSON.parse(account.get('account_data')) : null;
-  return obj;
+  let obj = {}
+  ;['id', 'account_id', 'account_type'].forEach((x) => (obj[x] = account.get(x)))
+  obj['account_data'] = !!account.get('account_data')
+    ? JSON.parse(account.get('account_data'))
+    : null
+  return obj
 }
 
 function serializeAmbassador(ambassador) {
-  let obj = {};
-  ['id', 'external_id', 'date_of_birth', 'first_name', 'last_name', 'phone', 'email', 'location', 'signup_completed', 'onboarding_completed', 'approved', 'locked', 'payout_provider', 'payout_additional_data', 'admin'].forEach(x => obj[x] = ambassador.get(x));
-  obj['address'] = !!ambassador.get('address') ? JSON.parse(ambassador.get('address').replace('#', 'no.')) : null;
-  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null;
-  obj['display_name'] = serializeName(ambassador.get('first_name'), ambassador.get('last_name'));
-  obj['quiz_results'] = !!ambassador.get('quiz_results') ? JSON.parse(ambassador.get('quiz_results')) : null;
+  let obj = {}
+  ;[
+    'id',
+    'external_id',
+    'date_of_birth',
+    'first_name',
+    'last_name',
+    'phone',
+    'email',
+    'location',
+    'signup_completed',
+    'onboarding_completed',
+    'approved',
+    'locked',
+    'payout_provider',
+    'payout_additional_data',
+    'admin',
+    'has_w9',
+  ].forEach((x) => (obj[x] = ambassador.get(x)))
+  obj['address'] = !!ambassador.get('address')
+    ? JSON.parse(ambassador.get('address').replace('#', 'no.'))
+    : null
+  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null
+  obj['display_name'] = serializeName(ambassador.get('first_name'), ambassador.get('last_name'))
+  obj['quiz_results'] = !!ambassador.get('quiz_results')
+    ? JSON.parse(ambassador.get('quiz_results'))
+    : null
 
-  let account = ambassador.get('owns_account').first();
-  obj['account'] = !!account ? serializeAccount(account.otherNode()) : null;
-  return obj;
+  let account = ambassador.get('owns_account').first()
+  obj['account'] = !!account ? serializeAccount(account.otherNode()) : null
+  console.log(obj)
+  return obj
 }
 
 function serializeAmbassadorForAdmin(ambassador) {
-  let obj = serializeAmbassador(ambassador);
-  obj.verification = ambassador.get('verification');
-  return obj;
+  let obj = serializeAmbassador(ambassador)
+  obj.verification = ambassador.get('verification')
+  return obj
 }
 
 function serializePayout(payout) {
@@ -53,60 +79,94 @@ function serializePayout(payout) {
     status: payout.get('status'),
     disbursement_id: payout.get('disbursement_id'),
     settlement_id: payout.get('settlement_id'),
-    disbursed_at: payout.get('disbursed_at') ? new Date(payout.get('disbursed_at').toString()) : null,
+    disbursed_at: payout.get('disbursed_at')
+      ? new Date(payout.get('disbursed_at').toString())
+      : null,
     settled_at: payout.get('settled_at') ? new Date(payout.get('settled_at').toString()) : null,
     error: payout.get('error') ? JSON.parse(payout.get('error')) : null,
     formatted_amount: payout.get('amount') ? formatNumber(payout.get('amount') / 100) : null,
-    formatted_disbursed_at: payout.get('disbursed_at') ? formatDate(new Date(payout.get('disbursed_at').toString())) : null,
-    formatted_settled_at: payout.get('settled_at') ? formatDate(new Date(payout.get('settled_at').toString())) : null,
-    account: payout.get('to_account') ? serializeAccount(payout.get('to_account')) : null
-  };
+    formatted_disbursed_at: payout.get('disbursed_at')
+      ? formatDate(new Date(payout.get('disbursed_at').toString()))
+      : null,
+    formatted_settled_at: payout.get('settled_at')
+      ? formatDate(new Date(payout.get('settled_at').toString()))
+      : null,
+    account: payout.get('to_account') ? serializeAccount(payout.get('to_account')) : null,
+  }
 }
 
 function serializeTriplerForCSV(tripler) {
-  let obj = {};
-  ['voter_id', 'id', 'first_name', 'last_name', 'status', 'phone', 'location', 'email', 'verification'].forEach(x => obj[x] = tripler.get(x));
-  obj['address'] = !!tripler.get('address') ? JSON.parse(tripler.get('address').replace('#', 'no.')) : null;
-  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null;
-  obj['display_name'] = serializeName(tripler.get('first_name'), tripler.get('last_name'));
-  obj['triplees'] = !!tripler.get('triplees') ? JSON.parse(tripler.get('triplees')) : null;
-  obj['is_ambassador_and_has_confirmed'] = tripler.get('is_ambassador_and_has_confirmed') ? true : false;
-  obj['is_ambassador'] = tripler.get('is_ambassador') ? true : false;
-  return obj;
+  let obj = {}
+  ;[
+    'voter_id',
+    'id',
+    'first_name',
+    'last_name',
+    'status',
+    'phone',
+    'location',
+    'email',
+    'verification',
+  ].forEach((x) => (obj[x] = tripler.get(x)))
+  obj['address'] = !!tripler.get('address')
+    ? JSON.parse(tripler.get('address').replace('#', 'no.'))
+    : null
+  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null
+  obj['display_name'] = serializeName(tripler.get('first_name'), tripler.get('last_name'))
+  obj['triplees'] = !!tripler.get('triplees') ? JSON.parse(tripler.get('triplees')) : null
+  obj['is_ambassador_and_has_confirmed'] = tripler.get('is_ambassador_and_has_confirmed')
+    ? true
+    : false
+  obj['is_ambassador'] = tripler.get('is_ambassador') ? true : false
+  return obj
 }
 
 function serializeTripler(tripler) {
-  let obj = {};
-  let ambassador = tripler.get('is_ambassador');
-  let was_once = ambassador ? ambassador.get('was_once') : null;
-  ['id', 'first_name', 'last_name', 'status', 'phone', 'location', 'email', 'age_decade'].forEach(x => obj[x] = tripler.get(x));
-  obj['address'] = !!tripler.get('address') ? JSON.parse(tripler.get('address')) : null;
-  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null;
-  obj['display_name'] = serializeName(tripler.get('first_name'), tripler.get('last_name'));
-  obj['display_phone'] = new PhoneNumber(`${tripler.get('phone')}`, 'US').a.number.national;
-  obj['triplees'] = !!tripler.get('triplees') ? JSON.parse(tripler.get('triplees')) : null;
-  obj['is_ambassador'] = ambassador ? true : false;
-  obj['is_ambassador_and_has_confirmed'] = tripler.get('is_ambassador_and_has_confirmed') ? true : false;
-  return obj;
+  let obj = {}
+  let ambassador = tripler.get('is_ambassador')
+  let was_once = ambassador ? ambassador.get('was_once') : null
+  ;['id', 'first_name', 'last_name', 'status', 'phone', 'location', 'email', 'age_decade'].forEach(
+    (x) => (obj[x] = tripler.get(x)),
+  )
+  obj['address'] = !!tripler.get('address') ? JSON.parse(tripler.get('address')) : null
+  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null
+  obj['display_name'] = serializeName(tripler.get('first_name'), tripler.get('last_name'))
+  obj['display_phone'] = new PhoneNumber(`${tripler.get('phone')}`, 'US').a.number.national
+  obj['triplees'] = !!tripler.get('triplees') ? JSON.parse(tripler.get('triplees')) : null
+  obj['is_ambassador'] = ambassador ? true : false
+  obj['is_ambassador_and_has_confirmed'] = tripler.get('is_ambassador_and_has_confirmed')
+    ? true
+    : false
+  return obj
 }
 
 function serializeNeo4JTripler(tripler) {
-  let obj = {};
-  ['distance', 'id', 'first_name', 'last_name', 'status', 'phone', 'location', 'email', 'age_decade'].forEach(x => obj[x] = tripler[x]);
-  obj['address'] = !!tripler.address ? JSON.parse(tripler.address) : null;
-  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null;
-  obj['display_name'] = serializeName(tripler.first_name, tripler.last_name);
-  obj['display_phone'] = new PhoneNumber(`${tripler.phone}`, 'US').a.number.national;
-  obj['triplees'] = !!tripler.triplees ? JSON.parse(tripler.triplees) : null;
-  return obj;
+  let obj = {}
+  ;[
+    'distance',
+    'id',
+    'first_name',
+    'last_name',
+    'status',
+    'phone',
+    'location',
+    'email',
+    'age_decade',
+  ].forEach((x) => (obj[x] = tripler[x]))
+  obj['address'] = !!tripler.address ? JSON.parse(tripler.address) : null
+  obj['display_address'] = !!obj['address'] ? serializeAddress(obj['address']) : null
+  obj['display_name'] = serializeName(tripler.first_name, tripler.last_name)
+  obj['display_phone'] = new PhoneNumber(`${tripler.phone}`, 'US').a.number.national
+  obj['triplees'] = !!tripler.triplees ? JSON.parse(tripler.triplees) : null
+  return obj
 }
 
 function serializeTriplee(triplee) {
-  return `${triplee.first_name} ${triplee.last_name}`;
+  return `${triplee.first_name} ${triplee.last_name}`
 }
 
 function serializeTripleeForCSV(triplee) {
-  return `${triplee.first_name} ${triplee.last_name} - ${triplee.housemate}`;
+  return `${triplee.first_name} ${triplee.last_name} - ${triplee.housemate}`
 }
 
 module.exports = {
@@ -120,5 +180,5 @@ module.exports = {
   serializeName: serializeName,
   serializeAddress: serializeAddress,
   serializeTriplee: serializeTriplee,
-  serializeTripleeForCSV: serializeTripleeForCSV
-};
+  serializeTripleeForCSV: serializeTripleeForCSV,
+}
