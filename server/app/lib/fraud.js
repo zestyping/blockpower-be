c5d364c336a4259bd1e1f8780363d09c846039ee
Ka-Ping Yee <zestyping@gmail.com>
2020-12-13 14:29:44 -0800

    Calculate the user's trust score from properties on the user.

--- server/app/lib/fraud.js
+++ server/app/lib/fraud.js
@@ -1,19 +1,47 @@
 import { ov_config } from './ov_config';
 
-// This should be a negative number.
+// The keys to this should be property names on the Ambassador, and
+// the values should be numeric weights.  The weights should be
+// POSITIVE if a positive value for the property means GOOD behaviour,
+// NEGATIVE if a positive value for the property means BAD behaviour.
+const TRUST_WEIGHTS = {
+  ekata_match_score: -1,
+  admin_bonus: 1,
+};
+
+// This should be a negative number, below which the user will be
+// locked for fraud.
 const FRAUD_THRESHOLD = parseFloat(ov_config.fraud_threshold) || -8;
 
 // Determines whether the user's actions should be blocked for fraud,
-// by summing all components of the fraud score.
+// by comparing the total trust score to the FRAUD_THRESHOLD.
 export const isLocked = (user) => {
   return user.locked;
 
-  // TODO: The above is a placeholder.  Ultimately this should look like:
-  const adminBonus = user.adminBonus || 0;
-  const niceTieBonus = 0;
-  const badHairPenalty = 0;
-  const emacsUserPenalty = 0; // just kidding!
+  // TODO: The above is a placeholder.  Ultimately this should be:
+  return calcTrust(user) < FRAUD_THRESHOLD;
+};
 
-  const score = adminBonus + niceTieBonus - badHairPenalty - emacsUserPenalty;
-  return score < FRAUD_THRESHOLD;
+// Calculates the total trust score, which will be compared against
+// the FRAUD_THRESHOLD.  POSITIVE means GOOD behaviour.
+export const calcTrust = (user) => {
+  const trust = 0;
+  for (const key of Object.keys(TRUST_WEIGHTS)) {
+    trust += (user[key] || 0) * TRUST_WEIGHTS[key];
+  }
+  return trust;
+};
+
+// Gets all the details on trust to send to the admin front-end for display.
+export const getTrustFactors = (user) => {
+  const factors = {};
+  for (const key of Object.keys(TRUST_WEIGHTS)) {
+    factors[key] = user[key] || 0;
+  }
+  const trust = calcTrust(user);
+  return {
+    factors: factors,
+    weights: TRUST_WEIGHTS,
+    trust: trust
+  };
 };
