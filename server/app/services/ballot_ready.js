b6c92401b0758cce22b78c9f1f38de7034caa033
Ka-Ping Yee <zestyping@gmail.com>
2021-01-02 21:32:09 -0800

    Send voters without addresses to the start of the BallotReady flow, which asks them for an address.

--- server/app/services/ballot_ready.js
+++ server/app/services/ballot_ready.js
@@ -1,54 +1,57 @@
 import { stringify } from 'query-string';
 import neode from '../lib/neode';
+import { parseJson } from '../lib/json';
 
-const BASE_URL = 'https://app.blockpower.civicengine.com/w/prepare';
+const START_URL = 'https://app.blockpower.civicengine.com/';
+const PREFILL_URL = 'https://app.blockpower.civicengine.com/w/prepare';
 
 const getFullName = (voter) => voter && [
   voter.get('first_name'), voter.get('last_name')
-].filter(x => x).join(' ');
+].filter(x => x).join(' ').trim();
 
 const getAddress = (person) => {
   if (!person) return null;
-  let address = {};
-  try {
-    address = JSON.parse(person.get('address'));
-  } catch (e) { }
-  let address1 = person.get('address1_with_house_number') ||  address.address1;
+  const address = parseJson(person.get('address'), {});
   return address ? [
-    address1,
+    person.get('address1_with_house_number') || address.address1,
     address.city,
     address.state,
     address.zip
-  ].join(', ') : null;
+  ].filter(x => x).join(', ').trim() : null;
 };
 
 const getZip = (person) => {
   if (!person) return null;
-  let address = {};
-  try {
-    address = JSON.parse(person.get('address'));
-  } catch (e) { }
+  const address = parseJson(person.get('address'), {});
   return (address ? address.zip : null) || person.get('zip');
 };
 
 const prepareBallotReadyUrl = (voter, canvasser, link_code) => {
-  let params;
-  params = {
+  const params = {
     name: getFullName(voter),
+    first_name: voter.get('first_name'),
+    last_name: voter.get('last_name'),
     // Let's leave out the email address for now because it was preloaded
     // with the Tripler, and it seems creepy to show them an e-mail address
     // from our files that they didn't enter themselves.
     // email: voter.get('email'),
     phone: voter?.get?.('phone'),
-    // If the voter doesn't have an address, use the zip code of the canvasser.
-    address: getAddress(voter) || getZip(canvasser),
+    address: getAddress(voter),
     utm_content: canvasser?.get?.('hs_id'),
     utm_term: voter?.get?.('hs_id'),
-    // The link_code param is not used by BallotReady; we include it so that
-    // it will show up in the initial_url field in the BallotReady clickstream.
+    // The link_code param is not used by BallotReady; it's included so it
+    // will show up in the initial_url field in the BallotReady clickstream.
     link_code: link_code
   };
-  return BASE_URL + '?' + stringify(params);
+  if (params.address) {
+    return PREFILL_URL + '?' + stringify(params);
+  } else {
+    // If we don't have an address, send the voter to START_URL, which will
+    // ask them to enter their address.  We still include all the params;
+    // even though BallotReady won't use them for prefilling, the URL will
+    // be captured in the initial_url field in the BallotReady clickstream.
+    return START_URL + '?' + stringify(params);
+  }
 };
 
 module.exports = {
